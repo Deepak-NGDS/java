f3ac50a2c9c35578a864462d8f7098820fe68d5c
swathi sajjan <swathi.novaglobal.in>
2025-05-20 13:50:11 +0530

    fetchtestdb made changes

--- fetchTestdb.js
+++ fetchTestdb.js
@@ -8,30 +8,17 @@ function delayTime(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
    }
 
-function logtotestdb({ delayTime, plant_id = 10, plant_name = 'log', comnc_date = '-', capacity = 0, message }) {
-    const timeStamp= new Date().toISOString()
-    const dateOnly = new Date(timeStamp).toISOString()
+function logtotestdb({ delayTime, plant_id = 10, plant_name = 'log', comnc_date = '2025-05-20', capacity = 0, message }) {
+    const timeStamp= Date.now()
+    const dateOnly = new Date(timeStamp).toISOString().slice(0,10)
     return createConnection(configj.dev_db)
     .then((connection)=>{
        return connection.execute(`INSERT INTO test_logs_swathi (ts,tdate,delay,plant_id,plant_name,comnc_date,capacity,description) VALUES (?,?,?,?,?,?,?,?)`, 
        [timeStamp,dateOnly, delayTime, plant_id, plant_name, comnc_date, capacity, message])
 
     })
-    .then(()=>
-    connection.end())
+   
 }
-
-// function afterFetch(Id, delayTime, message ,data) {
-//     const timeStamp= new Date().toISOString()
-//     const dateOnly = new Date(timeStamp).toISOString()
-//     return createConnection(configj.dev_db)
-//         .then(connection => {
-//             const sql = ` UPDATE test_logs_swathi SET ts = ?, tdate = ?, delay = ?, plant_name = ?, comnc_date = ?, capacity = ?, description = ? WHERE id = ?`
-//             const values =  [timeStamp,dateOnly, delayTime,data.plant_name,data.comnc_date,data.capacity,Id]
-//             return connection.query(sql,values)
-//         })
-// }
-
 
 function fetchPlantFromDev(id){
    return createConnection(configj.dev_db)
@@ -39,25 +26,21 @@ function fetchPlantFromDev(id){
        return connection.execute(`SELECT * FROM mas_sites WHERE id = ?`,[id])
 })
     .then(([rows])=>{
-        connection.end()
         return rows
 
-    })
+})
 } 
 
 function insertIntomydb(c){
    return createConnection(configj.my_db)
     .then((connection)=>{
-        const sql = `INSERT INTO mas_sites (id,name,region_id_fk, asset_type_id_fk, created_on,created_by_fk,comm_date,lati,longi,unit_price,site_capacity,power_metric_fk,
+        const sql = `INSERT IGNORE INTO mas_sites (id,name,region_id_fk, asset_type_id_fk, created_on,created_by_fk,comm_date,lati,longi,unit_price,site_capacity,power_metric_fk,
         address,spv_id_fk,utr_num,store_address,contact_num,fax_num,num_of_turbines,price_unit,feeder_id_fk,isDataCollectionEnabled,alias_name,timezone_id_fk,effeciency,
         def_air_density,hcode,isMetMastAvailable) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`
         const values = [c.id,c.name,c.region_id_fk, c.asset_type_id_fk, c.created_on,c.created_by_fk,c.comm_date,c.lati,c.longi,c.unit_price,c.site_capacity,c.power_metric_fk,
             c.address,c.spv_id_fk,c.utr_num,c.store_address,c.contact_num,c.fax_num,c.num_of_turbines,c.price_unit,c.feeder_id_fk,c.isDataCollectionEnabled,c.alias_name,c.timezone_id_fk,c.effeciency,
             c.def_air_density,c.hcode,c.isMetMastAvailable]
        return connection.query(sql,values)
-    })
-    .then(()=>{
-        connection.end()
     })
 
 }
@@ -73,13 +56,13 @@ function main(count){
     const id = currentid++
     const t1 = Date.now() % 2000;
 return delayTime(500)
-  .then(() => logtotestdb({ delayTime: 500, plant_id: 10, plant_name: 'log', comnc_date: '-', capacity: 0, message: `Going to fetch ${id} from devdb` }))
+  .then(() => logtotestdb({ delayTime: 500, plant_id: 10, plant_name: 'log', comnc_date: '2025-05-20' , capacity: 0, message: `Going to fetch ${id} from devdb` }))
   .then(() => delayTime(t1))
   .then(() => {
     const t2 = Date.now() % 2000;
     return fetchPlantFromDev(id).then((rows) => {
       if (rows.length === 0) {
-        return logtotestdb({ delayTime: t2, plant_id: 10, plant_name: 'log', comnc_date: '-', capacity: 0, message: `No such ${id} from devdb` })
+        return logtotestdb({ delayTime: t2, plant_id: 10, plant_name: 'log', comnc_date: '2025-05-20' , capacity: 0, message: `No such ${id} from devdb` })
           .then(() => allPlants());
       }
 
@@ -96,6 +79,7 @@ return delayTime(500)
   })
   .then(() => allPlants())
   .catch((error) => {
+    console.log(error)
     return allPlants()
   })
 }
