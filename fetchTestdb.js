7985e4dc02253038e13e7d78271767ec1c89a44c
swathi sajjan <swathi.novaglobal.in>
2025-05-21 09:41:35 +0530

    fetchtestdb made changes

--- fetchTestdb.js
+++ fetchTestdb.js
@@ -8,7 +8,7 @@ function delayTime(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
    }
 
-function delayTimelog(ms,plant_id = 10, plant_name = 'log', comnc_date = '2025-05-20', capacity = 0, message){
+function delayTimelog(ms,plant_id = 10, plant_name = 'log', comnc_date = '2025-05-20', capacity = 0, message=null){
     if (ms>100){
         return logtotestdb ({ delayTime: ms,plant_id,plant_name,comnc_date,capacity,message})
             .then(()=> delayTime(ms))
@@ -18,7 +18,7 @@ function delayTimelog(ms,plant_id = 10, plant_name = 'log', comnc_date = '2025-0
 
 }
 
-function logtotestdb({ delayTime, plant_id = 10, plant_name = 'log', comnc_date = '2025-05-20', capacity = 0, message }) {
+function logtotestdb({ delayTime, plant_id = 10, plant_name = 'log', comnc_date = '2025-05-20', capacity = 0, message=null }) {
     const offset = 330 * 60 * 1000;
     const timeStamp= Date.now() + offset;
     const dateOnly = new Date(timeStamp).toISOString()
@@ -67,7 +67,9 @@ function main(count){
    delayTimelog(500,10, 'log',  '2025-05-20', 0, `Stage 1 - Waiting for 500 ms before fetching the plant`)
   .then(() =>{
   return logtotestdb({ delayTime: 500, plant_id: 10, plant_name: 'log', comnc_date: '2025-05-20' , capacity: 0, message: `fetching the plant of ${id} from devdb` })})
-  .then(() => delayTimelog( Date.now() % 2000, 10,  'log', '2025-05-20',  0, `Stage 2 - Waiting for ${Date.now() % 2000} before going to fetch the plant of id ${id}`))
+  .then(() => {
+    const d1= Date.now() % 2000
+    return delayTimelog( d1, 10,  'log', '2025-05-20',  0, `Stage 2 - Waiting for ${Date.now() % 2000} before going to fetch the plant of id ${id}`)})
   .then(() => {
     const t2 = Date.now() % 2000;
     return fetchPlantFromDev(id)
@@ -80,13 +82,22 @@ function main(count){
       const row = rows[0];
 
       return logtotestdb({ delayTime: t2, plant_id: row.id, plant_name: row.name, comnc_date: row.comm_date, capacity: row.site_capacity, message: `Fetched the plant of ${id} from devdb` })
-        .then(() => delayTimelog(Date.now() % 2000, 10, 'log', '2025-05-20',0, `Stage 3 - Waiting for ${Date.now() % 2000} after fetching the plant of id ${id}`))
-        .then(() => logtotestdb({ delayTime: Date.now() % 2000, plant_id: row.id, plant_name: row.name, comnc_date: row.comm_date, capacity: row.site_capacity, message: `Going to insert the plant of ${id} into mydb` }))
-        .then(() => delayTime(Date.now() % 2000, 10,  'log',  '2025-05-20', 0, `Stage 4 - Waiting for ${Date.now() % 2000} before inserting the plant of id ${id}`))
+        .then(() =>{
+            const d2 = Date.now() % 2000
+            delayTimelog(d2, 10, 'log', '2025-05-20',0, `Stage 3 - Waiting for ${Date.now() % 2000} after fetching the plant of id ${id}`)
+            return logtotestdb({ delayTime: d2, plant_id: row.id, plant_name: row.name, comnc_date: row.comm_date, capacity: row.site_capacity, message: `Going to insert the plant of ${id} into mydb` })
+        })
+        .then(() => {
+            const d3 =Date.now() % 2000
+           return  delayTimelog(d3, 10,  'log',  '2025-05-20', 0, `Stage 4 - Waiting for ${Date.now() % 2000} before inserting the plant of id ${id}`)
+    })
         .then(() => insertIntomydb(row))
-        .then(() => delayTime(Date.now() % 2000, 10, 'log', '2025-05-20',  0, `Stage 5 - Waiting for ${Date.now() % 2000} after inserting the plant of id ${id}`))
-        .then(() => logtotestdb({ delayTime: Date.now() % 2000, plant_id: row.id, plant_name: row.name, comnc_date: row.comm_date, capacity: row.site_capacity, message: `Inserted the plant of ${id} into mydb` }));
-    });
+        .then(() => {
+            const d4 = Date.now() % 2000
+         delayTimelog(d4, 10, 'log', '2025-05-20',  0, `Stage 5 - Waiting for ${Date.now() % 2000} after inserting the plant of id ${id}`)
+       return logtotestdb({ delayTime: d4, plant_id: row.id, plant_name: row.name, comnc_date: row.comm_date, capacity: row.site_capacity, message: `Inserted the plant of ${id} into mydb` });
+    })
+})
   })
   .then(() => allPlants())
   .catch((error) => {
